9c6a383a0616b9213fea31cd6b42dad453256e04
daianh <115562284+daianh@users.noreply.github.com>
2022-11-04 20:51:20 +0700

    cmt

---
+++ demo.js
@@ -0,0 +1,16 @@
+function lastIndexOf(arr, elt, start = 1) {
+
+    for (let i = start - 1; i >= 0; i--)
+
+
+        if (arr[i] === elt) {
+            return i
+
+        }
+    return -1
+
+
+}
+
+
+console.log(lastIndexOf([1, 2, 1, 2], 2))
